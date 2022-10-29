1a5dfce260c80417af8ae04b043092dd55edfc12
Natali <nacas932@gmail.com>
2022-10-28 21:24:51 -0300

    agregue button y cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import OfertaInicio from "./components/OfferInitial/OfferInitial";
 import './App.css';
-
 import NavBar from './components/NavBar/NavBar';
+import ButtonTwo from "./components/ButtonTwo/ButtonTwo";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardsTwo from "./components/CardsTwo/CardsTwo";
+
+
+
 function App() {
   return (
     <div className="App">
@@ -11,8 +16,10 @@ function App() {
       <div>
         <OfertaInicio texto={"Free Delivery over $100. Gift with Purchase over $150"} />
         <NavBar></NavBar>
+        <ButtonTwo>comprar</ButtonTwo>
+       <CardsTwo />
       </div>
-
+     
     </div>
     </div>
   )
---
+++ src/components/ButtonTwo/ButtonTwo.jsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+
+function ButtonTwo() {
+  return (
+    <>
+      <Button variant="dark">Vender</Button> <Button variant="link">Link</Button>
+    </>
+  );
+}
+
+export default ButtonTwo;
+
