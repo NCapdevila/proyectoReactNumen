bb840dbfa804e1111b5def960a04999de994b715
Ramiro Capdevila <ramirocapdevila23@gmail.com>
2022-11-02 01:13:25 -0300

    Agrego breakpoint de 576 para el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -35,32 +35,32 @@ function Footer(){
                 </div>
             </div>
             <div className='col'>
-                <div>
+                
                 <ul className='container-list-info'>
-                    <li>Para más información, atención personalizada o consultas, contactanos:</li>
+                    <li>Atención personalizada o consultas, contactanos:</li>
                 </ul>
-
                 <ul className='container-list-info'>
                     <li>+598 96 300 060</li>
                     <li>Lunes a viernes de 9 a 23hs | Sábados de 10 a 18hs</li>   
                     <li>Domingos de 10 a 14hs</li>
                     <li>contacto@vopero.com </li>     
                 </ul>   
-                    
                 <ul className='container-list-info'>
                     <li>Marco Bruto 1369, Montevideo, Uruguay</li>
                     <li>Lunes a viernes de 10 a 19hs | Sábados de 9 a 13hs</li> 
                 </ul>
-                </div>
+                
             </div>
             </div>
             
             <div className='container-bottom-footer'>
                 <div className='container-links-footer'>
+                   
                     SEGUINOS
                     <a href=""><img src={ig} alt="" className='img-icons-footer' /></a>
                     <a href=""><img src={fb} alt="" className='img-icons-footer' /></a>
                     <a href=""><img src={linkedin} alt="" className='img-icons-footer' /></a>
+                    
                 </div>
                 <div className='container-copy'>
                 &copy;{new Date().getFullYear()}, VOPERO | Todos los derechos reservados
