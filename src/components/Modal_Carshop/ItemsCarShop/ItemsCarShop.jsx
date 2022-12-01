f6687a6d2a63da4e49e4b1d97eb372c9c9579e07
Matias Leonardo Bobi <matiasleonardobobi@gmail.com>
2022-11-30 22:24:39 -0300

    Cambios con localstorage

--- src/components/Modal_Carshop/ItemsCarShop/ItemsCarShop.jsx
+++ src/components/Modal_Carshop/ItemsCarShop/ItemsCarShop.jsx
@@ -1,47 +1,29 @@
-import useFetchData from '../../../hooks/UseFetchData'
-import useDeleteData from "../../../hooks/UseDeleteData";
 import ItemCart from "./ItemCart/ItemCart";
 
 const ItemsCarShop = (props) => {
-  const { refetch,refetchData, url } = props;
-  const {
-    data: forms,
-    isLoading: isDataLoading,
-    error: dataError,
-  } = useFetchData(url, refetch)
 
-  const {
-    deleteData,
-    successDelete: issuccessDelete,
-    error: DeletionError,
-  } = useDeleteData(url, refetchData, "Hubo un error al intentar borrar el item")
+  const deleteData = () =>{
+    
+  }
+
+  const data = JSON.parse(localStorage.getItem("itemsprueba"))
   return (
     <div>
-    <div>
-      
-      {
-      dataError.IsError ? 
-      <div>Hubo un error al intentar cargar los íems del carrito</div> 
-      :
-      isDataLoading && !dataError.IsError ? 
-      "Cargando items"
-      :
-      forms.map(form =>(
-        <ItemCart 
-        name={form.type}
-        brand={form.brand}
-        price={form.price}
-        quantity={form.quantity}
-        deleteData={deleteData}
-        id={form.id}
-        />
-      ))
-      }
-        
-    </div>
-    <div>{issuccessDelete ? "" :
-    <div>Se borró un ítem del carrito</div>
-      }</div>
+      <div>
+        {
+          data.map(data =>(
+            <ItemCart 
+            name={data.user}
+            brand={data.brand}
+            price={data.price}
+            quantity={data.quantity}
+            finalquantity={data.quantityfinally}
+            id={data.id}
+            />
+          ))
+          
+        }
+      </div>
     </div>
   );
 };
