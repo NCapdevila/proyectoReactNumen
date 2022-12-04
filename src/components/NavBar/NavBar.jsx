c6b0549fe978f328e92dc4c4ecac906eb176105f
Matias Leonardo Bobi <matiasleonardobobi@gmail.com>
2022-12-04 10:19:46 -0300

    Limpinado warning de la consola

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -42,7 +42,7 @@ const NavBar = () => {
         </div>
         <div className='lateralNavBar__container'>
         <div className='searchNavBar__container'>
-        <svg width="25" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.934 18.75a7.875 7.875 0 100-15.75 7.875 7.875 0 000 15.75zM16.502 16.443L21.058 21" stroke="#989BA0" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+        <svg width="25" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.934 18.75a7.875 7.875 0 100-15.75 7.875 7.875 0 000 15.75zM16.502 16.443L21.058 21" stroke="#989BA0" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
           <input className='inputNavBar' type="text" placeholder="Buscar productos..."/>
         </div>
 
